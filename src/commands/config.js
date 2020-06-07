d931c2495cae998b75284035373e44542d133a62
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-06-07 23:43:57 +0700

    Simplify config setup command

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,6 +1,33 @@
-exports.command = 'config <command>';
-exports.desc = 'Set CloudFlare Token';
-exports.builder = function (yargs) {
-	return yargs.commandDir('config_cmd');
+import inquirer from 'inquirer';
+import log from '../utils/logger';
+import { defaultConfig, write } from '../utils/config';
+import { validateNotEmpty } from '../utils/promp-validators';
+
+exports.command = 'config setup';
+exports.desc = 'Setup Cloudflare API Token';
+exports.builder = {};
+exports.handler = async function () {
+	try {
+		const defaults = defaultConfig();
+
+		const questions = [
+			{
+				name: 'apiToken',
+				type: 'input',
+				message: 'Your Cloudflare API Token: ',
+				default: '',
+				validate: validateNotEmpty,
+			},
+		];
+
+		const answers = await inquirer.prompt(questions);
+
+		const config = Object.assign(defaults, answers);
+
+		await write(config);
+
+		log.success('Successfully saved api token file');
+	} catch (err) {
+		log.error(err);
+	}
 };
-exports.handler = {};
